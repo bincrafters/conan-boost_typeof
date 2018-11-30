f957b98e2c4ee38530ab90721b5551ce8c132219
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:13 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostTypeofConan(base.BoostBaseConan):
     name = "boost_typeof"
@@ -18,5 +18,3 @@ class BoostTypeofConan(base.BoostBaseConan):
         "boost_preprocessor",
         "boost_type_traits"
     ]
-
-
